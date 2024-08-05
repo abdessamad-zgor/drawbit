28a35d93fdb7a68ba877bb209443a9145184c59a
Abdessamad Zgor <abdessamadzgor@gmail.com>
2024-08-05 10:27:59 +0100

    make it overlay ?

--- src/views/New.tsx
+++ src/views/New.tsx
@@ -5,26 +5,22 @@ import React from 'react'
 function New() {
   const { createNewScene, onDimensionsChange, onUnitChange, newDimensions, newUnit } = useScene()
   return (
-    <div className='w-full min-h-screen flex flex-col items-center justify-center bg-stone-700'>
+    <div className='absolute inset-0 z-90 w-full min-h-screen flex flex-col items-center justify-center bg-stone-200/30'>
       <Card className='w-4/12' >
         <CardContent className=' bg-white'>
           <CardHeader>
             <CardTitle>Create a new scene</CardTitle>
-            <CardDescription>Set new scene dimensions.</CardDescription>
+            <CardDescription>Set Grid Size.</CardDescription>
           </CardHeader>
           <form className='w-full' onSubmit={createNewScene}>
             <div className='flex w-full gap-2'>
               <div className='w-2/5 flex flex-col'>
-                <label className=''>Width</label>
+                <label className=''>Grid Width</label>
                 <input className='border border-stone-400 w-full p-1 rounded' type="number" name="dem-x" value={newDimensions[0]} onChange={onDimensionsChange} />
               </div>
               <div className='w-2/5 flex flex-col'>
-                <label>Height</label>
+                <label>Grid Height</label>
                 <input className='border border-stone-400 w-full p-1 rounded' type="number" name="dem-y" value={newDimensions[1]} onChange={onDimensionsChange} />
-              </div>
-              <div className='w-1/5 flex flex-col'>
-                <label className=''>Unit</label>
-                <input className='border border-stone-400 w-full p-1 rounded' type="number" value={newUnit} onChange={onUnitChange} />
               </div>
             </div>
             <div className='flex justify-end py-1'>
